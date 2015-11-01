a2a0454b93c2bb0725b036dad0b2b246b278bbd0
Ullarah <phil@mcau.ullarah.com>
2015-11-01 12:18:37 +1000

    uLib: Javadoc'd CommonString

--- shared/uLib/src/main/java/com/ullarah/ulib/function/CommonString.java
+++ shared/uLib/src/main/java/com/ullarah/ulib/function/CommonString.java
@@ -7,33 +7,79 @@ import org.bukkit.plugin.Plugin;
 
 public class CommonString {
 
+    /**
+     * The name of the plugin placed in prefix format
+     *
+     * @param plugin the plugin object
+     * @return a fancy prefix style string
+     */
     public static String pluginPrefix(Plugin plugin) {
 
         return ChatColor.GOLD + "[" + plugin.getName() + "] " + ChatColor.WHITE;
 
     }
 
+    /**
+     * A permission denied message
+     *
+     * @param plugin the plugin object
+     * @param sender the sender who receives the message
+     */
     public static void messagePermDeny(Plugin plugin, CommandSender sender) {
 
         sender.sendMessage(pluginPrefix(plugin) + ChatColor.RED + "No permission.");
 
     }
 
+    /**
+     * A no console usage message
+     *
+     * @param plugin the plugin object
+     * @param sender the sender who receives the message
+     */
     public static void messageNoConsole(Plugin plugin, CommandSender sender) {
 
         sender.sendMessage(pluginPrefix(plugin) + ChatColor.RED + "No console usage.");
 
     }
 
+    /**
+     * An under maintenance message
+     *
+     * @param plugin the plugin object
+     * @param sender the sender who receives the message
+     */
     public static void messageMaintenance(Plugin plugin, CommandSender sender) {
 
         sender.sendMessage(pluginPrefix(plugin) + ChatColor.RED + "Currently under maintenance.");
 
     }
 
-    public static void messagePlayer(Plugin plugin, Player player, String[] messages) {
+    /**
+     * A message that is sent to a {@code CommandSender} object
+     *
+     * @param plugin   the plugin object
+     * @param sender   the sender who receives the message
+     * @param prefix   the prefix of the current plugin
+     * @param messages an array of messages to send
+     */
+    public static void messageSend(Plugin plugin, CommandSender sender, boolean prefix, String[] messages) {
 
-        for (String message : messages) player.sendMessage(pluginPrefix(plugin) + message);
+        for (String message : messages) sender.sendMessage(prefix ? pluginPrefix(plugin) + message : message);
+
+    }
+
+    /**
+     * A message that is sent to a {@code Player} object
+     *
+     * @param plugin   the plugin object
+     * @param player   the player who receives the message
+     * @param prefix   the prefix of the current plugin
+     * @param messages an array of messages to send
+     */
+    public static void messageSend(Plugin plugin, Player player, boolean prefix, String[] messages) {
+
+        for (String message : messages) player.sendMessage(prefix ? pluginPrefix(plugin) + message : message);
 
     }
 
