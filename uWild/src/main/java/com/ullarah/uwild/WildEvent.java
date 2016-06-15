dfa9b780c5f1e9466d71f0515727da6b81a8a1a4
Ullarah <phil@mcau.ullarah.com>
2016-06-15 12:02:47 +1000

    uWild: Only spawn on specific reasons  - Added more entity spawns

--- uWild/src/main/java/com/ullarah/uwild/WildEvent.java
+++ uWild/src/main/java/com/ullarah/uwild/WildEvent.java
@@ -1,49 +1,122 @@
 package com.ullarah.uwild;
 
-import org.bukkit.Location;
-import org.bukkit.TreeType;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import static org.bukkit.event.entity.CreatureSpawnEvent.*;
 
 public class WildEvent implements Listener {
 
     @EventHandler
     public void mobSpawn(final CreatureSpawnEvent event){
 
-        Entity entity = event.getEntity();
+        List<SpawnReason> spawnReasons = new ArrayList<>();
+        spawnReasons.add(SpawnReason.NATURAL);
+        spawnReasons.add(SpawnReason.SPAWNER_EGG);
+        spawnReasons.add(SpawnReason.CHUNK_GEN);
 
-        if( new Random().nextInt(99) + 1 > 50 ) {
+        if (spawnReasons.contains(event.getSpawnReason())) {
 
-            entity.setMetadata("uWild", new FixedMetadataValue(WildInit.getPlugin(), true));
+            if( new Random().nextInt(99) + 1 > 50 ) {
 
-            switch (event.getEntityType()) {
+                Entity entity = event.getEntity();
 
-                case CREEPER:
-                    entity.setCustomName("Treeper");
-                    break;
+                Boolean isWild = false;
+                String entityWildType = "";
+                List<String> entityArray = new ArrayList<>();
 
-                case CHICKEN:
-                    entity.setCustomName("Unstable Chicken");
-                    break;
+                switch (event.getEntityType()) {
 
-                case SHEEP:
-                    entity.setCustomName("Time Sheep");
-                    break;
+                    case SPIDER:
+                        isWild = true;
+                        entityWildType = ChatColor.YELLOW + "Spyker";
+                        break;
 
-                case COW:
-                    entity.setCustomName("Cow of Zeus");
-                    break;
+                    case CREEPER:
+                        isWild = true;
+                        entityArray.add(ChatColor.GREEN + "Treeper");
+                        entityArray.add(ChatColor.BLUE + "H2O Creeper");
+                        entityWildType = entityArray.get(new Random().nextInt(entityArray.size()));
+                        break;
+
+                    case CHICKEN:
+                        isWild = true;
+                        entityWildType = ChatColor.RED + "Unstable Chicken";
+                        break;
+
+                    case SHEEP:
+                        isWild = true;
+                        entityWildType = ChatColor.LIGHT_PURPLE + "Time Sheep";
+                        break;
+
+                    case COW:
+                        isWild = true;
+                        entityWildType = ChatColor.AQUA + "Cow of Zeus";
+                        break;
+
+                    case PIG:
+                        isWild = true;
+                        entityWildType = ChatColor.LIGHT_PURPLE + "Oinkers";
+                        break;
+
+                }
+
+                if (isWild) {
+
+                    entity.setMetadata("uWild", new FixedMetadataValue(WildInit.getPlugin(), true));
+                    entity.setCustomName(entityWildType);
+                    entity.setCustomNameVisible(true);
+
+                    switch (event.getEntityType()) {
+
+                        case SPIDER:
+                            if (entity.getCustomName().matches(ChatColor.YELLOW + "Spyker")) {
+
+                                Entity passenger = entity.getWorld().spawnEntity(entity.getLocation(), EntityType.CAVE_SPIDER);
+
+                                passenger.setSilent(true);
+                                passenger.setCustomName(ChatColor.YELLOW + "Spyker");
+                                passenger.setCustomNameVisible(true);
+
+                                entity.setPassenger(passenger);
+                                entity.setSilent(true);
+
+                            }
+                            break;
+
+                        case PIG:
+                            if (entity.getCustomName().matches(ChatColor.LIGHT_PURPLE + "Oinkers")) {
+
+                                Entity pigOne = entity.getWorld().spawnEntity(entity.getLocation(), EntityType.PIG);
+                                Entity pigTwo = entity.getWorld().spawnEntity(entity.getLocation(), EntityType.PIG);
+
+                                pigOne.setCustomName(ChatColor.LIGHT_PURPLE + "Oinkers");
+                                pigTwo.setCustomName(ChatColor.LIGHT_PURPLE + "Oinkers");
+
+                                pigOne.setCustomNameVisible(true);
+                                pigTwo.setCustomNameVisible(true);
+
+                                pigOne.setPassenger(pigTwo);
+                                entity.setPassenger(pigOne);
+
+                            }
+                            break;
+
+                    }
+
+                }
 
             }
-
-            entity.setCustomNameVisible(true);
 
         }
 
@@ -56,28 +129,47 @@ public class WildEvent implements Listener {
         Location location = entity.getLocation();
         World world = location.getWorld();
 
+        int locX = (int) location.getX();
+        int locY = (int) location.getY();
+        int locZ = (int) location.getZ();
+
         if(entity.hasMetadata("uWild")) switch (event.getEntityType()) {
 
             case CREEPER:
-                if (entity.getCustomName().equalsIgnoreCase("Treeper")) {
+                if (entity.getCustomName().matches(ChatColor.GREEN + "Treeper")) {
+
                     world.createExplosion(location, 0.0F);
                     world.generateTree(location, new Random().nextInt(10) == 5 ? TreeType.BIG_TREE : TreeType.TREE);
+
+                }
+
+                if (entity.getCustomName().matches(ChatColor.BLUE + "H2O Creeper")) {
+
+                    world.createExplosion(location, 0.0F);
+                    world.getBlockAt(locX, locY, locZ).setType(Material.WATER);
+                    world.getBlockAt(locX, locY + 1, locZ).setType(Material.WATER);
+
                 }
                 break;
 
             case CHICKEN:
-                if (entity.getCustomName().equalsIgnoreCase("Unstable Chicken"))
+                if (entity.getCustomName().matches(ChatColor.RED + "Unstable Chicken"))
                     world.createExplosion(location, 2.0F, true);
                 break;
 
             case SHEEP:
-                if (entity.getCustomName().equalsIgnoreCase("Time Sheep"))
-                    world.setTime(new Random().nextInt(10));
+                if (entity.getCustomName().matches(ChatColor.LIGHT_PURPLE + "Time Sheep"))
+                    world.setTime(new Random().nextInt(23000));
                 break;
 
             case COW:
-                if (entity.getCustomName().equalsIgnoreCase("Cow of Zeus"))
+                if (entity.getCustomName().matches(ChatColor.AQUA + "Cow of Zeus"))
                     world.strikeLightning(location);
+                break;
+
+            case PIG:
+                if (entity.getCustomName().matches(ChatColor.LIGHT_PURPLE + "Oinkers"))
+                    world.spawnParticle(Particle.DRAGON_BREATH, location, 500, 3.0, 3.0, 3.0);
                 break;
 
         }
