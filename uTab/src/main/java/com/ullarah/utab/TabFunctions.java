6eeb02d058f5c15758d9c8a9ac9c4b10ae4c750a
RobotoRaccoon <RobotoRaccoon@gmail.com>
2019-04-26 15:00:16 +1000

    uTab: Update to 1.14

--- uTab/src/main/java/com/ullarah/utab/TabFunctions.java
+++ uTab/src/main/java/com/ullarah/utab/TabFunctions.java
@@ -1,12 +1,12 @@
 package com.ullarah.utab;
 
-import net.minecraft.server.v1_13_R2.IChatBaseComponent;
-import net.minecraft.server.v1_13_R2.PacketPlayOutPlayerListHeaderFooter;
-import net.minecraft.server.v1_13_R2.PlayerConnection;
+import net.minecraft.server.v1_14_R1.IChatBaseComponent;
+import net.minecraft.server.v1_14_R1.PacketPlayOutPlayerListHeaderFooter;
+import net.minecraft.server.v1_14_R1.PlayerConnection;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
